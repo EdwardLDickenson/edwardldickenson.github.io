f302220af603d35e6c1a56851ef962e11f41081e
EdwardDickenson <edwardldickenson@gmail.com>
2017-06-30 06:57:38 -0500

    Initial

---
+++ breakout/breakout.js
@@ -0,0 +1,179 @@
+var levelOne = [{"dimensions":[600,600,40,20,80,20,2,5,6]},{"type":1,"hits":1,"x":0,"y":20},{"type":1,"hits":1,"x":0,"y":0},{"type":1,"hits":1,"x":0,"y":60},{"type":1,"hits":1,"x":0,"y":80},{"type":1,"hits":1,"x":0,"y":40},{"type":1,"hits":1,"x":0,"y":100},{"type":1,"hits":1,"x":0,"y":140},{"type":1,"hits":1,"x":0,"y":120},{"type":1,"hits":1,"x":0,"y":160},{"type":1,"hits":1,"x":0,"y":200},{"type":1,"hits":1,"x":0,"y":220},{"type":1,"hits":1,"x":0,"y":180},{"type":1,"hits":1,"x":40,"y":220},{"type":1,"hits":1,"x":40,"y":200},{"type":1,"hits":1,"x":40,"y":180},{"type":1,"hits":1,"x":40,"y":160},{"type":1,"hits":1,"x":40,"y":140},{"type":1,"hits":1,"x":40,"y":100},{"type":1,"hits":1,"x":40,"y":60},{"type":1,"hits":1,"x":40,"y":20},{"type":1,"hits":1,"x":40,"y":0},{"type":1,"hits":1,"x":40,"y":80},{"type":1,"hits":1,"x":40,"y":40},{"type":1,"hits":1,"x":40,"y":120},{"type":1,"hits":1,"x":80,"y":0},{"type":1,"hits":1,"x":80,"y":40},{"type":1,"hits":1,"x":80,"y":80},{"type":1,"hits":1,"x":80,"y":140},{"type":1,"hits":1,"x":80,"y":200},{"type":1,"hits":1,"x":80,"y":220},{"type":1,"hits":1,"x":80,"y":180},{"type":1,"hits":1,"x":80,"y":160},{"type":1,"hits":1,"x":80,"y":120},{"type":1,"hits":1,"x":80,"y":100},{"type":1,"hits":1,"x":80,"y":60},{"type":1,"hits":1,"x":80,"y":20},{"type":1,"hits":1,"x":120,"y":0},{"type":1,"hits":1,"x":120,"y":20},{"type":1,"hits":1,"x":120,"y":40},{"type":1,"hits":1,"x":120,"y":60},{"type":1,"hits":1,"x":120,"y":80},{"type":1,"hits":1,"x":120,"y":100},{"type":1,"hits":1,"x":120,"y":120},{"type":1,"hits":1,"x":120,"y":140},{"type":1,"hits":1,"x":120,"y":160},{"type":1,"hits":1,"x":120,"y":180},{"type":1,"hits":1,"x":120,"y":200},{"type":1,"hits":1,"x":120,"y":220},{"type":1,"hits":1,"x":160,"y":220},{"type":1,"hits":1,"x":200,"y":220},{"type":1,"hits":1,"x":240,"y":220},{"type":1,"hits":1,"x":280,"y":220},{"type":1,"hits":1,"x":320,"y":220},{"type":1,"hits":1,"x":360,"y":220},{"type":1,"hits":1,"x":400,"y":220},{"type":1,"hits":1,"x":440,"y":220},{"type":1,"hits":1,"x":480,"y":220},{"type":1,"hits":1,"x":520,"y":220},{"type":1,"hits":1,"x":560,"y":220},{"type":1,"hits":1,"x":560,"y":200},{"type":1,"hits":1,"x":520,"y":200},{"type":1,"hits":1,"x":480,"y":200},{"type":1,"hits":1,"x":440,"y":200},{"type":1,"hits":1,"x":400,"y":200},{"type":1,"hits":1,"x":360,"y":200},{"type":1,"hits":1,"x":320,"y":200},{"type":1,"hits":1,"x":280,"y":200},{"type":1,"hits":1,"x":240,"y":200},{"type":1,"hits":1,"x":200,"y":200},{"type":1,"hits":1,"x":160,"y":200},{"type":1,"hits":1,"x":160,"y":180},{"type":1,"hits":1,"x":200,"y":180},{"type":1,"hits":1,"x":240,"y":180},{"type":1,"hits":1,"x":280,"y":180},{"type":1,"hits":1,"x":320,"y":180},{"type":1,"hits":1,"x":360,"y":180},{"type":1,"hits":1,"x":400,"y":180},{"type":1,"hits":1,"x":440,"y":180},{"type":1,"hits":1,"x":480,"y":180},{"type":1,"hits":1,"x":520,"y":180},{"type":1,"hits":1,"x":560,"y":180},{"type":1,"hits":1,"x":560,"y":160},{"type":1,"hits":1,"x":520,"y":160},{"type":1,"hits":1,"x":480,"y":160},{"type":1,"hits":1,"x":400,"y":160},{"type":1,"hits":1,"x":440,"y":160},{"type":1,"hits":1,"x":360,"y":160},{"type":1,"hits":1,"x":320,"y":160},{"type":1,"hits":1,"x":280,"y":160},{"type":1,"hits":1,"x":240,"y":160},{"type":1,"hits":1,"x":200,"y":160},{"type":1,"hits":1,"x":160,"y":160},{"type":1,"hits":1,"x":160,"y":140},{"type":1,"hits":1,"x":200,"y":140},{"type":1,"hits":1,"x":240,"y":140},{"type":1,"hits":1,"x":280,"y":140},{"type":1,"hits":1,"x":320,"y":140},{"type":1,"hits":1,"x":360,"y":140},{"type":1,"hits":1,"x":400,"y":140},{"type":1,"hits":1,"x":440,"y":140},{"type":1,"hits":1,"x":480,"y":140},{"type":1,"hits":1,"x":520,"y":140},{"type":1,"hits":1,"x":560,"y":140},{"type":1,"hits":1,"x":560,"y":120},{"type":1,"hits":1,"x":520,"y":120},{"type":1,"hits":1,"x":480,"y":120},{"type":1,"hits":1,"x":440,"y":120},{"type":1,"hits":1,"x":400,"y":120},{"type":1,"hits":1,"x":360,"y":120},{"type":1,"hits":1,"x":320,"y":120},{"type":1,"hits":1,"x":280,"y":120},{"type":1,"hits":1,"x":240,"y":120},{"type":1,"hits":1,"x":200,"y":120},{"type":1,"hits":1,"x":160,"y":120},{"type":1,"hits":1,"x":160,"y":100},{"type":1,"hits":1,"x":200,"y":100},{"type":1,"hits":1,"x":240,"y":100},{"type":1,"hits":1,"x":280,"y":100},{"type":1,"hits":1,"x":320,"y":100},{"type":1,"hits":1,"x":360,"y":100},{"type":1,"hits":1,"x":400,"y":100},{"type":1,"hits":1,"x":440,"y":100},{"type":1,"hits":1,"x":480,"y":100},{"type":1,"hits":1,"x":520,"y":100},{"type":1,"hits":1,"x":560,"y":100},{"type":1,"hits":1,"x":560,"y":80},{"type":1,"hits":1,"x":520,"y":80},{"type":1,"hits":1,"x":480,"y":80},{"type":1,"hits":1,"x":440,"y":80},{"type":1,"hits":1,"x":400,"y":80},{"type":1,"hits":1,"x":360,"y":80},{"type":1,"hits":1,"x":320,"y":80},{"type":1,"hits":1,"x":280,"y":80},{"type":1,"hits":1,"x":240,"y":80},{"type":1,"hits":1,"x":200,"y":80},{"type":1,"hits":1,"x":160,"y":80},{"type":1,"hits":1,"x":160,"y":40},{"type":1,"hits":1,"x":200,"y":60},{"type":1,"hits":1,"x":240,"y":60},{"type":1,"hits":1,"x":160,"y":60},{"type":1,"hits":1,"x":200,"y":40},{"type":1,"hits":1,"x":240,"y":40},{"type":1,"hits":1,"x":280,"y":60},{"type":1,"hits":1,"x":280,"y":40},{"type":1,"hits":1,"x":320,"y":60},{"type":1,"hits":1,"x":360,"y":60},{"type":1,"hits":1,"x":400,"y":60},{"type":1,"hits":1,"x":440,"y":60},{"type":1,"hits":1,"x":480,"y":60},{"type":1,"hits":1,"x":520,"y":60},{"type":1,"hits":1,"x":560,"y":60},{"type":1,"hits":1,"x":560,"y":40},{"type":1,"hits":1,"x":520,"y":40},{"type":1,"hits":1,"x":440,"y":40},{"type":1,"hits":1,"x":360,"y":40},{"type":1,"hits":1,"x":320,"y":40},{"type":1,"hits":1,"x":400,"y":40},{"type":1,"hits":1,"x":480,"y":40},{"type":1,"hits":1,"x":560,"y":20},{"type":1,"hits":1,"x":560,"y":0},{"type":1,"hits":1,"x":520,"y":20},{"type":1,"hits":1,"x":520,"y":0},{"type":1,"hits":1,"x":480,"y":0},{"type":1,"hits":1,"x":480,"y":20},{"type":1,"hits":1,"x":440,"y":20},{"type":1,"hits":1,"x":440,"y":0},{"type":1,"hits":1,"x":400,"y":0},{"type":1,"hits":1,"x":400,"y":20},{"type":1,"hits":1,"x":360,"y":20},{"type":1,"hits":1,"x":360,"y":0},{"type":1,"hits":1,"x":320,"y":0},{"type":1,"hits":1,"x":320,"y":20},{"type":1,"hits":1,"x":280,"y":20},{"type":1,"hits":1,"x":280,"y":0},{"type":1,"hits":1,"x":240,"y":0},{"type":1,"hits":1,"x":240,"y":20},{"type":1,"hits":1,"x":200,"y":20},{"type":1,"hits":1,"x":200,"y":0},{"type":1,"hits":1,"x":160,"y":0},{"type":1,"hits":1,"x":160,"y":20},{"type":1,"hits":1,"x":0,"y":240},{"type":1,"hits":1,"x":40,"y":240},{"type":1,"hits":1,"x":120,"y":240},{"type":1,"hits":1,"x":80,"y":240},{"type":1,"hits":1,"x":160,"y":240},{"type":1,"hits":1,"x":200,"y":240},{"type":1,"hits":1,"x":240,"y":240},{"type":1,"hits":1,"x":280,"y":240},{"type":1,"hits":1,"x":320,"y":240},{"type":1,"hits":1,"x":360,"y":240},{"type":1,"hits":1,"x":400,"y":240},{"type":1,"hits":1,"x":480,"y":240},{"type":1,"hits":1,"x":520,"y":240},{"type":1,"hits":1,"x":560,"y":240},{"type":1,"hits":1,"x":440,"y":240}];
+
+var levels = [levelOne];
+
+//	These are just the default values incase the array does not load for some reason
+var canvasWidth = 600;
+var canvasHeight = 600;
+var blockWidth = 40;
+var blockHeight = 20;
+var paddleWidth = 80;
+var paddleHeight = 20;
+
+var levelDimensions = [];
+var blocks = [];
+var levelName = "";
+
+var timer;
+
+var paddle = {"xv": 0};
+var ball = {"xv": 0, "yv": 0};
+var shouldLoadLevel = true;
+var context;
+
+document.onkeydown = kvent;
+
+function kvent(evt)
+{
+	if(evt.keyCode == '37')
+	{
+		console.log("left arrow");
+		paddle.xv -= levelDimensions[6];
+	}
+
+	if(evt.keyCode == '39')
+	{
+		console.log("right arrow");
+		paddle.xv += levelDimensions[6];
+	}
+}
+
+function renderRect(x, y, width, height, str)
+{
+	context.beginPath();
+	context.rect(x, y, width, height);
+	context.fillStyle = str;
+	context.fill();
+	context.closePath();
+}
+
+function renderType(x, y, n)
+{
+	if(n == 1)
+	{
+		renderRect(x, y, levelDimensions[2], levelDimensions[3], "#FF0000");
+		renderRect(x + 4, y + 4, levelDimensions[2] - 8, levelDimensions[3] - 8, "#880000");
+	}
+
+	if(n == 2)
+	{
+		renderRect(x, y, levelDimensions[2], levelDimensions[3], "#00FF00");
+		renderRect(x + 4, y + 4, levelDimensions[2] - 8, levelDimensions[3] - 8, "#008800");
+	}
+
+	if(n == 3)
+	{
+		renderRect(x, y, levelDimensions[2], levelDimensions[3], "#0000FF");
+		renderRect(x + 4, y + 4, levelDimensions[2] - 8, levelDimensions[3] - 8, "#000088");
+	}
+}
+
+function render()
+{
+	fillScreen("#FFFFFF");
+
+	renderRect(paddle.x, paddle.y, levelDimensions[4], levelDimensions[5], "#FF00FF");
+
+	for(var i = 0; i < blocks.length; ++i)
+	{
+		renderType(blocks[i].x, blocks[i].y, blocks[i].type);
+	}
+}
+
+function collisionRectWall(x, y, w, h)
+{
+	if(x < 0 || y < 0 || x > levelDimensions[0] || y + h > levelDimensions[1])
+	{
+		return true;
+	}
+
+	return false;
+}
+
+function move()
+{
+	if(!collisionRectWall(paddle.x, paddle.y, levelDimensions[4], levelDimensions[5]) &&
+		!collisionRectWall(paddle.x + paddle.xv, paddle.y, levelDimensions[4], levelDimensions[5]))
+	{
+		paddle.x += paddle.xv;
+	}
+
+	else
+	{
+		paddle.xv = 0;
+	}
+}
+
+function main()
+{
+	render();
+	move();
+
+	//	Pull information from the level array
+	if(shouldLoadLevel)
+	{
+		var level = levels[0];
+
+		//	Level dimensions
+		levelDimensions.push(600);
+		levelDimensions.push(600);
+		levelDimensions.push(level[0]["dimensions"][2]);
+		levelDimensions.push(level[0]["dimensions"][3]);
+		levelDimensions.push(level[0]["dimensions"][4]);
+		levelDimensions.push(level[0]["dimensions"][5]);
+		levelDimensions.push(level[0]["dimensions"][6]);
+		levelDimensions.push(level[0]["dimensions"][7]);
+		levelDimensions.push(level[0]["dimensions"][8]);
+
+		//	Now pull the blocks from the level data as well
+		for(var i = 1; i < levelOne.length; ++i)
+		{
+			var block = {};
+			block.type = levelOne[i].type;
+			block.hits = levelOne[i].hits;
+			block.x = levelOne[i].x;
+			block.y = levelOne[i].y;
+
+			blocks.push(block);
+		}
+
+		//	Set up objects to move around
+		paddle.x = ((levelDimensions[0] / 2) - (levelDimensions[4] / 2));
+		paddle.y = levelDimensions[1] - (levelDimensions[5]);
+
+		shouldLoadLevel = false;
+	}
+}
+
+function createContext()
+{
+	var canvas = document.getElementById("main.focus.canvas");
+	return canvas.getContext("2d");
+}
+
+function fillScreen(str)
+{
+	var context = createContext();
+	context.rect(0, 0, canvasWidth, canvasHeight);
+	context.fillStyle = str;
+	context.fill();
+}
+
+function blockTypeOne(x, y)
+{
+	var block;
+	block.x = x;
+	block.y = y;
+	block.width = 20;
+	block.height = 10;
+
+	return block;
+}
+
+window.onload = function(){
+	console.log("Javascript file loaded correctly");
+	fillScreen("#ffffff");
+	context = document.getElementById("main.focus.canvas").getContext("2d");
+
+	timer = setInterval(main, 50);
+};
---
+++ breakout/breakoutdesigner.js
@@ -0,0 +1,493 @@
+var levelDimensions = [];
+
+var type = 1;
+var showGrid = false;
+var levelName = "";
+
+var blocks = [];
+var redoStack = [];
+
+var context;
+
+function createContext()
+{
+	var canvas = document.getElementById("main.focus.canvas");
+	return canvas.getContext("2d");
+}
+
+/*
+	In the interest of efficiency, context should not be created but should
+	rather be a variable
+*/
+function fillScreen(str)
+{
+	context.rect(0, 0, levelDimensions[0], levelDimensions[1]);
+	context.fillStyle = str;
+	context.fill();
+}
+
+function renderRect(x, y, width, height, str)
+{
+	context.beginPath();
+	context.rect(x, y, width, height);
+	context.fillStyle = str;
+	context.fill();
+	context.closePath();
+}
+
+function renderType(x, y, n)
+{
+	//	The logic for these four conditions is the same, the only difference
+	//	is
+	if(n == 1)
+	{
+		renderRect(x, y, levelDimensions[2], levelDimensions[3], "#FF0000");
+		renderRect(x + 4, y + 4, levelDimensions[2] - 8, levelDimensions[3] - 8, "#880000");
+	}
+
+	if(n == 2)
+	{
+		renderRect(x, y, levelDimensions[2], levelDimensions[3], "#00FF00");
+		renderRect(x + 4, y + 4, levelDimensions[2] - 8, levelDimensions[3] - 8, "#008800");
+	}
+
+	if(n == 3)
+	{
+		renderRect(x, y, levelDimensions[2], levelDimensions[3], "#0000FF");
+		renderRect(x + 4, y + 4, levelDimensions[2] - 8, levelDimensions[3] - 8, "#000088");
+	}
+}
+
+function renderGrid()
+{
+	for(var i = 0; i < levelDimensions[0]; i += levelDimensions[2])
+	{
+		context.beginPath();
+		context.moveTo(i, 0);
+		context.lineTo(i, levelDimensions[1]);
+		context.strokeStyle = "#000000";
+		context.stroke();
+	}
+
+	for(var i = 0; i < levelDimensions[1]; i += levelDimensions[3])
+	{
+		context.beginPath();
+		context.moveTo(0, i);
+		context.lineTo(levelDimensions[0], i);
+		context.strokeStyle = "#000000";
+		context.stroke();
+	}
+}
+
+function render()
+{
+	fillScreen("#FFFFFF");
+
+	if(showGrid)
+	{
+		renderGrid();
+	}
+
+	for(var i = 0; i < blocks.length; ++i)
+	{
+		renderType(blocks[i].x, blocks[i].y, blocks[i].type);
+	}
+}
+
+/*
+	Changing button color can probably be acheived by changing classes
+*/
+
+function changeButtonColor()
+{
+	//	This method is slightly inefficient since it writes and overwrites the
+	//	same button twice. But only for one button, so it's not a big deal
+	$("#main\\.focus\\.typeone").css({"color": "white", "background-color": "black"});
+	$("#main\\.focus\\.typetwo").css({"color": "white", "background-color": "black"});
+	$("#main\\.focus\\.typethree").css({"color": "white", "background-color": "black"});
+}
+
+function typeOne()
+{
+	console.log("Type one was clicked");
+	type = 1;
+
+	changeButtonColor();
+	$("#main\\.focus\\.typeone").css({"color": "black", "background-color": "white"});
+}
+
+function typeTwo()
+{
+	console.log("Type two was clicked");
+	type = 2;
+
+	changeButtonColor();
+	$("#main\\.focus\\.typetwo").css({"color": "black", "background-color": "white"});
+}
+
+function typeThree()
+{
+	console.log("Type three was clicked");
+	type = 3;
+
+	changeButtonColor();
+	$("#main\\.focus\\.typethree").css({"color": "black", "background-color": "white"});
+}
+
+function normalizePosition(x, y)
+{
+	x = Math.floor(x / levelDimensions[2]) * levelDimensions[2];
+	y = Math.floor(y / levelDimensions[3]) * levelDimensions[3];
+
+	return {"x": x, "y": y};
+}
+
+function getIndexByXY(array, x, y)
+{
+	//	A k-d Tree might be useful for this, but the domain is so small that it
+	//	isn't really worth it.
+	for(var i = 0; i < array.length; ++i)
+	{
+		if(array[i].x == x && array[i].y == y)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+function locationFilled(x, y)
+{
+	if(getIndexByXY(blocks, x, y) != -1)
+	{
+		console.log("Colision between block to be added and existing block");
+		return true;
+	}
+
+	return false;
+}
+
+function updateLiId()
+{
+	var list = $("#main\\.sidebarleft\\.panel\\.list").children();
+
+	for(var i = 0; i < list.length; ++i)
+	{
+		var x = list[i].children[1].value;
+		var y = list[i].children[2].value;
+
+		list[i].id = i;
+	}
+}
+
+function updateLi()
+{
+	console.log("Update");
+
+	//	Create new block
+	var children = $(this).parent().children();
+	var type = children[0].value;
+	var x = children[1].value;
+	var y = children[2].value;
+
+	var position = normalizePosition(x, y);
+	x = position.x;
+	y = position.y;
+
+	if(getIndexByXY(x, y) != -1)
+	{
+		console.log("Collision");
+		return;
+	}
+
+	var parent = $(this).parent();
+	var id = parent.attr("id");
+
+	console.log(type);
+	blocks[id].type = type;
+	blocks[id].x = x;
+	blocks[id].y = y;
+}
+
+function delLi(block)
+{
+	console.log("Delete");
+
+	if(block.x !== undefined)
+	{
+		var list = $("#main\\.sidebarleft\\.panel\\.list").children();
+
+		//	Once again, it's probably just easier to use DOM here instead of
+		//	casting back to JQuery or similar
+		for(var i = 0; i < list.length; ++i)
+		{
+			var x = list[i].children[1].value;
+			var y = list[i].children[2].value;
+			if(block.x == x && block.y == y)
+			{
+				list[i].parentNode.removeChild(list[i]);
+			}
+		}
+		return;
+	}
+
+	var parent = $(this).parent();
+
+	//	x.value is not JQuery, it's DOM
+	var x = parent.children()[1].value;
+	var y = parent.children()[2].value;
+	var index = getIndexByXY(blocks, x, y);
+
+	redoStack.push(blocks[index]);
+	blocks.splice(index, 1);
+
+	parent.remove();
+	updateLiId();
+}
+
+function createPanelElement(block)
+{
+	var li = $("<li id='" + String(getIndexByXY(blocks, block.x, block.y) + "'>"));
+	li.append("Type: ");
+	li.append("<input type='text' name='type' maxlength='1' size='1' value='" + String(block.type) + "'></input>");
+	li.append("Horizontal: ");
+	li.append("<input type='text' name='x' maxlength='3' size='4' value='" + String(block.x) + "'></input>");
+	li.append("Vertical: ");
+	li.append("<input type='text' name='y' maxlength='3' size='4' value='" + String(block.y) + "'></input>");
+	li.append("<br>");
+
+	var update = $("<input type='button' value='Update'/>");
+	var del = $("<input type='button' value='Delete'/>");
+	update.click(updateLi);
+	del.click(delLi);
+
+	li.append(update);
+	li.append(del);
+
+	$("#main\\.sidebarleft\\.panel\\.list").append(li);
+}
+
+function canvasMouseDown(evt)
+{
+	console.log("Mouse down event");
+
+	var canvas = document.getElementById("main.focus.canvas");
+	var x = evt.pageX - canvas.offsetLeft;
+	var y = evt.pageY - canvas.offsetTop;
+	var hits = type;
+
+	if(hits == 3)
+	{
+		hits = -1;
+	}
+
+	var position = normalizePosition(x, y);
+	x = position.x;
+	y = position.y;
+
+	if(locationFilled(x, y))
+	{
+		return;
+	}
+
+	blocks.push({"type": type, "hits": hits, "x": x, "y": y});
+	redoStack = [];
+	createPanelElement(blocks[blocks.length - 1]);
+	console.log(blocks[blocks.length - 1]);
+}
+
+function canvasMouseUp(evt)
+{
+	//console.log("Mouse up event");
+
+	var canvas = document.getElementById("main.focus.canvas");
+	var x = evt.pageX - canvas.offsetLeft;
+	var y = evt.pageY - canvas.offsetTop;
+	var hits = type;
+
+	if(hits == 3)
+	{
+		hits = -1;
+	}
+
+	var position = normalizePosition(x, y);
+	x = position.x;
+	y = position.y;
+
+	/*if(locationFilled(x, y))
+	{
+		return;
+	}*/
+
+	//	Not implemented yet
+}
+
+//	Note that this function does not actually look at the value of the checkbox,
+//	it only toggles a bool true and false
+function toggleGrid()
+{
+	console.log("Show Grid: " + !showGrid);
+	showGrid = !showGrid;
+}
+
+function undo()
+{
+	if(blocks.length === 0)
+	{
+		return;
+	}
+
+	console.log("Undo button was pressed");
+	delLi(blocks[blocks.length - 1]);
+	redoStack.push(blocks[blocks.length - 1]);
+	blocks.splice(blocks.length - 1);
+}
+
+function redo()
+{
+	if(redoStack.length === 0)
+	{
+		return;
+	}
+
+	console.log("Redo button was pressed");
+	blocks.push(redoStack[redoStack.length - 1]);
+	createPanelElement(redoStack[redoStack.length - 1]);
+	redoStack.splice(redoStack.length - 1);
+}
+
+function downloadLevel(evt)
+{
+	console.log("Downloading");
+
+	var str = "";
+
+	var element = document.createElement('a');
+
+	//var levelData = levelDimensions.concat(blocks);
+	var levelData = [];
+	levelData.push({"dimensions": levelDimensions});
+	levelData = levelData.concat(blocks);
+
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(levelData)));
+	element.setAttribute('download', levelName + ".txt");
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+
+	evt.preventDefault();
+}
+
+function setLevelName()
+{
+	levelName = $("#main\\.top\\.name").val();
+}
+
+function updateValues(evt)
+{
+	console.log("Update was pressed");
+
+	var updatedVariables = []
+
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.screenwidth").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.screenheight").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.blockwidth").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.blockheight").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.paddlewidth").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.paddleheight").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.speed").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.xv").val());
+	updatedVariables.push($("#main\\.sidebarright\\.form\\.field\\.yv").val());
+
+	for(var i = 0; i < updatedVariables.length; ++i)
+	{
+		if(updatedVariables[i] != "" && !isNaN(updatedVariables[i]))
+		{
+			console.log(updatedVariables[i]);
+			levelDimensions[i] = updatedVariables[i];
+		}
+	}
+
+	evt.preventDefault();
+}
+
+//	Note that currently there is no validation for errata in the level file
+function upload(evt)
+{
+	var file = this.files[0];
+
+	levelName = file.name.substring(0, file.name.length - 4);
+	$("#main\\.top\\.name").val(levelName);
+
+	var reader = new FileReader();
+		reader.onload = function(progressEvent){
+
+		var info = JSON.parse(this.result);
+
+		var newDimensions = info[0].dimensions;
+
+		for(var i = 0; i < newDimensions; ++i)
+		{
+			levelDimensions[i] = newDimensions[i];
+		}
+
+		blocks = [];
+		for(var i = 1; i < info.length; ++i)
+		{
+			var block = {};
+			block.type = info[i].type;
+			block.hits = info[i].hits;
+			block.x = info[i].x;
+			block.y = info[i].y;
+
+			blocks.push(block);
+		}
+	};
+
+	reader.readAsText(file);
+}
+
+window.onload = function(){
+	console.log("Javascript file loaded correctly");
+	context = document.getElementById("main.focus.canvas").getContext("2d");
+
+	levelDimensions.push(600);	//	canvasWidth
+	levelDimensions.push(600);	//	canvasHeight
+	levelDimensions.push(40);	//	blockWidth
+	levelDimensions.push(20);	//	blockHeight
+	levelDimensions.push(80);	//	paddleWidth
+	levelDimensions.push(20);	//	paddleHeight
+	levelDimensions.push(2);	//	paddleSpeed
+	levelDimensions.push(5);	//	ballXV
+	levelDimensions.push(6);	//	ballYV
+
+	typeOne();
+	setLevelName();
+
+	//	Instead of setting an interval and rewriting the same pixels with the
+	//	same color, it might be better to write a "should change function"
+	timer = setInterval(render, 100);
+	$("#main\\.top\\.button").click(setLevelName);
+	$("#main\\.sidebarleft\\.grid").click(toggleGrid);
+	$("#main\\.sidebarleft\\.undo").click(undo);
+	$("#main\\.sidebarleft\\.redo").click(redo);
+	$("#main\\.focus\\.typeone").click(typeOne);
+	$("#main\\.focus\\.typetwo").click(typeTwo);
+	$("#main\\.focus\\.typethree").click(typeThree);
+	$("#main\\.focus\\.download").click(downloadLevel);
+	$("#main\\.focus\\.canvas").mousedown(canvasMouseDown);
+	$("#main\\.focus\\.canvas").mouseup(canvasMouseUp);
+	$("#main\\.focus\\.upload").change(upload);
+	$("#main\\.sidebarright\\.form").submit(updateValues);
+};
+
+
+//	TODO:
+//	Grid breaks when changing block dimensions. Screen resize not implemented
+//
+//
